5723eaf8c13f8c72553c51d80c3403fb9c740028
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2015-10-02 11:47:00 +0200

    First commit

---
+++ DataWeather/app/src/main/java/com/mcmu/juanjesus/dataweather/LoginActivity.java
@@ -0,0 +1,191 @@
+package com.mcmu.juanjesus.dataweather;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity implements LocationListener {
+
+    @Bind(R.id.loginUsernameEditText)protected EditText userNameText;
+    @Bind(R.id.loginLetsGoButton)protected Button letsGoBtn;
+    @Bind(R.id.loginYourLocationTextView) protected TextView yourLocationTextView;
+
+    private LocationManager locationManager;
+    private String provider;
+
+    private static final int ONE_MINUTES = 1000 * 60;
+    private static final int FIVE_SECONDS = 1000 * 5;
+    private static final int FIVE_METERS = 5;
+
+    //region Activity lifecycle
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Butterknife injection
+        ButterKnife.bind(this);
+
+        // Get location service ref
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Criteria criteria = new Criteria();
+        criteria.setCostAllowed(false);
+        criteria.setAltitudeRequired(false);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        provider = locationManager.getBestProvider(criteria, false);
+
+        locationManager.requestLocationUpdates(provider, FIVE_SECONDS, FIVE_METERS, this);
+        Location lastLocation = locationManager.getLastKnownLocation(provider);
+
+        // onLocationChanged will realize about the changes
+        if (lastLocation != null) {
+            String yourLocation = getString(R.string.your_location) + ": " + lastLocation.getLatitude() + ", " + lastLocation.getLongitude();
+            yourLocationTextView.setText(yourLocation);
+            String city = getLocationName(lastLocation);
+            yourLocationTextView.append("(" + city + ")");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    // -------------------------------------------------------------------> Activity running
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+
+        locationManager.removeUpdates(this);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    //endregion Activity lifecycle
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.loginLetsGoButton)
+    public void LetsGoBtnClicked(Button target) {
+        if (userNameText.length() == 0) {
+            Toast.makeText(this, getString(R.string.cannot_empty_name), Toast.LENGTH_SHORT).show();
+        } else {
+            Intent weatherListActivityIntent = new Intent(this, WeatherListActivity.class);
+            startActivity(weatherListActivityIntent);
+        }
+    }
+
+
+    //region LocationListener
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("Login:onLocationChanged", location.toString());
+        if (location != null) {
+            String yourLocation = getString(R.string.your_location) + ": " + location.getLatitude() + ", " + location.getLongitude();
+            yourLocationTextView.setText(yourLocation);
+            String city = getLocationName(location);
+            yourLocationTextView.append("(" + city + ")");
+        } else {
+            yourLocationTextView.setText(getString(R.string.location_not_found));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Login:onStatusChanged", provider);
+}
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Login:onStatusChanged", provider);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Login:onStatusChanged", provider);
+    }
+    //endregion LocationListener
+
+
+    //region Geocoder
+    private String getLocationName(Location location) {
+
+        String result = "";
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses.size() > 0) {
+                result = addresses.get(0).getLocality();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("DW:getLocationName", result);
+        return result;
+    }
+    //endregion Geocoder
+}
---
+++ DataWeather/app/src/main/java/com/mcmu/juanjesus/dataweather/WeatherListActivity.java
@@ -0,0 +1,153 @@
+package com.mcmu.juanjesus.dataweather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class WeatherListActivity extends AppCompatActivity {
+
+    //region Activity lifecycle
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Button v = new Button(this);
+        setContentView(v);
+        /*setContentView(R.layout.activity_login);
+
+        final ListView listView = (ListView)findViewById(R.id.listView);
+        final EditText text = (EditText)findViewById(R.id.todoText);
+        final Button button = (Button)findViewById(R.id.addButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Firebase("https://dataweather.firebaseio.com/todoItems")
+                        .push()
+                        .child("text")
+                        .setValue(text.getText().toString());
+            }
+        });
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+
+        listView.setAdapter(adapter);
+
+        Firebase.setAndroidContext(this);
+
+        new Firebase("https://dataweather.firebaseio.com/todoItems")
+                .addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        adapter.add((String) dataSnapshot.child("text").getValue());
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        adapter.remove((String) dataSnapshot.child("text").getValue());
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+                });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                new Firebase("https://dataweather.firebaseio.com/todoItems")
+                        .orderByChild("text")
+                        .equalTo((String)listView.getItemAtPosition(position))
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.hasChildren()) {
+                                    DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                                    firstChild.getRef().removeValue();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(FirebaseError firebaseError) {
+
+                            }
+                        });
+            }
+        });*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    // -------------------------------------------------------------------> Activity running
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    //endregion Activity lifecycle
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
