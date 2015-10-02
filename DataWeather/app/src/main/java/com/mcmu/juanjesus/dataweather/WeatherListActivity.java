7e4bdc73e4b75ed74c8807ca4b3c5e68537a11de
Juan Jesus Izquierdo Domenech <juanjesusizquierdodomenech@gmail.com>
2015-10-02 14:24:09 +0200

    Add menu to weather list activity

--- DataWeather/app/src/main/java/com/mcmu/juanjesus/dataweather/WeatherListActivity.java
+++ DataWeather/app/src/main/java/com/mcmu/juanjesus/dataweather/WeatherListActivity.java
@@ -1,5 +1,8 @@
 package com.mcmu.juanjesus.dataweather;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -23,7 +26,8 @@ public class WeatherListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.activity_weatherlist);
+
         /*setContentView(R.layout.activity_login);
 
         final ListView listView = (ListView)findViewById(R.id.listView);
